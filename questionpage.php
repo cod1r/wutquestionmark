c8d449007cb97b71e27a4d4ce6add1f285b2dc5a
cod1r <jasonho353@gmail.com>
2020-05-27 03:46:28 -0500

    need to work on post redirect after answers and better implement 404

--- questionpage.php
+++ questionpage.php
@@ -8,9 +8,7 @@ function load_question($bool=FALSE){
         echo htmlspecialchars($_GET['question']);
     }
     else{
-        http_response_code(404);
-        header('HTTP/1.1 404 Not Found');
-        echo "404<br><h2 style='text-align: center;'>Wut?</h2>";
+        header("Location ./NotFound.php");
     }
 }
 
@@ -27,4 +25,26 @@ function verifyquestion(){
     load_question(False);
     return False;
 }
+
+function load_answers($bool=FALSE){
+    GLOBAL $dbh;
+    if($bool){
+        $stmt = $dbh->prepare("SELECT * FROM questionsanswered WHERE question=?");
+        $stmt->execute(array($_GET['question']));
+        foreach($stmt->fetchAll() as $row){
+            echo '<div id="answer" style="border: 1px solid grey;border-radius: 5px; ">
+                        <h5 style="text-align: left; border-bottom: 1px solid grey; background-color: #5bc0de; padding: 5px">'. $row['username'] .'</h5>
+                            <p style="text-align: left; padding: 0px 5px 0px 5px;">'
+                                . $row['answer'] .
+                             '</p>
+                     </div>';
+        }
+    }
+    
+}
+if(isset($_POST['answerToQ'])){
+    $stmt = $dbh->prepare("INSERT INTO questionsanswered (answer, question, username) VALUES (?, ?, ?)");
+    $stmt->execute(array($_POST['answerToQ'], $_GET['question'], $_SESSION['username']));
+}
+
 ?>
--- questionpage/questionpage.js
+++ questionpage/questionpage.js
@@ -7,3 +7,18 @@ document.getElementById('answerNot').onclick = function disappear(){
     document.getElementById('answerArea').style.display = 'none';
     document.getElementById('answerButton').style.display = 'inline';
 }
+
+
+function refreshAnswers(){
+    var xmlhttp = new XMLHttpRequest ();
+    xmlhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var parser = new DOMParser();
+            document.getElementById('answers').innerHTML = parser.parseFromString(this.responseText, "text/html");
+        }
+    }
+    xmlhttp.open("GET", "questionpage.php?question="+document.getElementById('TheQuestion').innerHTML,true);
+    xmlhttp.send();
+}
+
+// setInterval(refreshAnswers, 5000);
