da81cb58970a83259e0ad6ed9a70d2cde0d5e462
cod1r <jasonho353@gmail.com>
2020-06-09 14:25:54 -0500

    redesigned login page

--- README.md
+++ README.md
@@ -4,4 +4,11 @@
 - check if username already exists
 - design home page
 - look into hashing passwords and personal info in database
-- implement answers into the database
+- Redo navigation bar
+- implement remember me
+
+## Thoughts on the what the Wu? website should be focused on and how it should work
+
+- Core subjects such as math, english, social studies, and science.
+- Categories should be picked when asking.
+- Subjects could be divided up into other sub topics.
--- includes/nav_bar.php
+++ includes/nav_bar.php
@@ -1,37 +1,33 @@
       <?php  
-      echo '<nav class="navbar navbar-expand-lg navbar-light bg-info">
-            <a class="navbar-brand text-dark" href="#">Wut?</a>
+      echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="border-radius: 20px; margin: 5px;">
+            <a class="navbar-brand" href="#">Wu?</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-          
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                  <a class="nav-link text-dark" href="/index.php">Home</a>
+                  <a class="nav-link" href="/index.php">Home</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link text-dark" href="/login.php">Profile</a>
+                <li class="nav-item active">
+                  <a class="nav-link" href="/login.php">Profile</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link text-dark" href="./ask.php">Ask</a>
+                <li class="nav-item active">
+                  <a class="nav-link" href="./ask.php">Ask</a>
                 </li>
-                <li class="nav-item dropdown">
+                <li class="nav-item dropdown active">
                   <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Options
+                    Categories
                   </a>
                   <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <a class="dropdown-item" href="#">Math</a>
+                    <a class="dropdown-item" href="#">Science</a>
+                    <a class="dropdown-item" href="#">English</a>
+                    <a class="dropdown-item" href="#">Social Studies</a>
+                    <a class="dropdown-item" href="#">Computer Science</a>
                   </div>
                 </li>
               </ul>
-              <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
-              </form>
             </div>
           </nav>'
           ?>
