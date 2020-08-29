c800b077c19d05a56c6977d4acfeddf3df2ce48d
cod1r <jasonho353@gmail.com>
2020-08-29 18:57:45 -0500

    added hashing for passwords

--- login.php
+++ login.php
@@ -11,7 +11,7 @@ require './mysql.php';
 
 if (isset($_POST['username']) && isset($_POST['password'])){
     foreach(($dbh->query("SELECT username, password FROM credentials")) as $row){
-        if ($row["username"] == $_POST["username"] && $row["password"] == $_POST["password"]){
+        if ($row["username"] == $_POST["username"] && password_verify($_POST["password"], $row["password"])){
             $stmt2 = $dbh->prepare("UPDATE credentials SET sessionID = ? WHERE username =  ? AND password = ?"); 
             $stmt2->execute(array(session_id(), $row["username"], $row["password"]));
             $_SESSION["username"] = $row["username"];
--- register.php
+++ register.php
@@ -4,7 +4,7 @@ require './register/register.html';
 
 if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST["email"])){
     $stmt = $dbh->prepare("INSERT INTO credentials (username, password, sessionID , email) VALUES (?, ?, ?, ?)");
-    $stmt->execute(array($_POST['username'], $_POST['password'], session_id(), $_POST['email']));
+    $stmt->execute(array($_POST['username'], password_hash($_POST['password'], PASSWORD_DEFAULT), session_id(), $_POST['email']));
     $_SESSION["username"] = $_POST["username"];
     $_SESSION["authenticated"] = true;
     ob_clean();
