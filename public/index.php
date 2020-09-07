e9ef95f23268359d80f63c99197dd9d0f5d7a642
cod1r <jasonho353@gmail.com>
2020-09-07 02:46:17 -0500

    added a better homepage

--- public/index.php
+++ public/index.php
@@ -1,4 +1,12 @@
 <?php 
-require '../session.php';
+require '../dbfile.php';
 require '../homepage/home.html'; 
+
+function count_questions(){
+    $questions = array();
+    GLOBAL $dbh;
+    $result = pg_query($dbh, "SELECT question FROM questionsasked");
+    $result = pg_fetch_all($result, 1);
+    echo count($result);
+}
 ?>
